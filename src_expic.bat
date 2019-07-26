962eeade8c275ab94c7eaa64cd0b96fea28cf7c6
Olin Lathrop <OlinLathrop@users.noreply.github.com>
2019-07-26 13:16:35 -0400

    Removed C18 capability from SRC_EXPIC.
    The code to handle builds including C18 code was obsolete anyway, and
    would not have worked as it was.  If C18 capability is required in the
    future, support for it needs to be added differently from what was
    removed.

--- src_expic.bat
+++ src_expic.bat
@@ -7,10 +7,6 @@ rem   directory.  The second is the generic name of the PIC executable.  The
 rem   executable will be built in the appropriate SRC directory.
 rem
 setlocal
-if "%C18Dir%"=="" (
-  call treename_var "%MPLABDir%/../c18" C18Dir
-  )
-
 call src_get %1 %2.linkpic
 copya %2.linkpic %2.lkr
 
@@ -18,20 +14,6 @@ copya -out %2.lkr -append -s "LIBPATH ."
 if exist %2.o      copya -out %2.lkr -append -s "FILES %2.o"
 if exist %2_strt.o copya -out %2.lkr -append -s "FILES %2_strt.o"
 if exist %2.lib    copya -out %2.lkr -append -s "FILES %2.lib"
-
-if not "%clibpic18%"=="" (
-  call godir "(cog)src/%1"
-  if not exist %2_c018i.o (
-    call src_get pic c018i.c18
-    call godir "(cog)src/%1"
-    copya "(cog)src/pic/c018i.c18" %2_c018i.c18
-    "%C18Dir%\bin\mcc18" %2_c018i.c18 -I="%C18Dir%\h" -fo=%2_c018i.o
-    if not exist %2_c018i.o exit /b 3
-    )
-  copya -out %2.lkr -append -s "FILES %2_c018i.o"
-  copya -out %2.lkr -append -s "FILES %C18Dir%\lib\p%clibpic18%.lib"
-  copya -out %2.lkr -append -s "FILES %C18Dir%\lib\clib.lib"
-  )
 
 if exist %2.out del %2.out
 if exist %2.cof del %2.cof
