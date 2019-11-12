3965d94d4f4b63cfd5e4b9bc3b28859d0d9ee90f
Olin Lathrop <OlinLathrop@users.noreply.github.com>
2019-11-12 11:24:33 -0500

    Added EMBED_DEL_OBSOLETE.BAT.
    This script deletes files from previous Embed software installations
    that should no longer be present in new installations.  For example,
    if a new program is created that replaces a previous BAT file, the old
    BAT file must be deleted from new installations that include the new
    program.
    
    The intent is to keep this script up to date as files are obsoleted.

---
+++ embed_del_obsolete.bat
@@ -0,0 +1,7 @@
+@echo off
+rem
+rem   Remove files that might have been installed previously, but that should
+rem   not be installed anymore.
+rem
+call delete (cog)bat/plotfilt.bat
+call delete (cog)com/plotfilt.bat
