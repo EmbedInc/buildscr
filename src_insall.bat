8c7fde3291ff1a0a8f293eff8b948e9cf1344cb4
Olin Lathrop <OlinLathrop@users.noreply.github.com>
2019-12-09 11:32:00 -0500

    SRC_INSALL removed attempt to build <name>2.ins.pas file.

--- src_insall.bat
+++ src_insall.bat
@@ -3,13 +3,13 @@ rem
 rem   SRC_INSALL <src dir lnam> <.INSALL.PAS generic name>
 rem
 rem   Build the .INSALL.PAS file.  This produces the C .H include file and
-rem   leaves it in the SRC directory.  It also puts public and private include
-rem   files for the library into the SRC directory.
+rem   leaves it in the SRC directory.
 rem
-call src_get %1 %2.ins.pas
-call src_get %1 %22.ins.pas
-
+rem   For the <name>.INSALL.PAS file, the contents of the <name>.INS.PAS file
+rem   will be exported to the <name>.H file.
+rem
 if exist %2.h del %2.h
 call src_get %1 %2.insall.pas
+call src_get %1 %2.ins.pas
 sst %2.insall.pas -show_unused 0 -local_ins -ins %2.ins.pas
 rename %2.insall.c %2.h
