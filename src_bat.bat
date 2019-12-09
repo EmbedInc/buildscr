1444f855c7d2ddd61d3455a6fa7334817ca7af36
Olin Lathrop <OlinLathrop@users.noreply.github.com>
2019-12-09 16:29:14 -0500

    SRC_BAT now announces the file name.
    Also unified features between SRC_BAT and SRC_ESCR.

--- src_bat.bat
+++ src_bat.bat
@@ -2,16 +2,13 @@
 rem
 rem   SRC_BAT <batfile gnam>
 rem
-rem   "Build" the BAT file by installing it into the appropriate directory.
+rem   Build the BAT script by installing it into the appropriate directory.
 rem
 setlocal
 call src_get "%srcdir%" "%~1.bat"
-
-call treename_var "(cog)bat" tnam
-if exist "%tnam%" goto :havedir
-call treename_var "(cog)com" tnam
-:havedir
+call treename_var (cog)bat tnam
+if not exist "%tnam%" call treename_var (cog)com tnam
 if not exist "%tnam%" mkdir "%tnam%"
-
-copya "%~1.bat" "%tnam%/%~1.bat"
+echo %~1.bat
+copya -in "%~1.bat" -out "%tnam%/%~1.bat"
 del "%~1.bat"
--- src_escr.bat
+++ src_escr.bat
@@ -2,11 +2,13 @@
 rem
 rem   SRC_ESCR <escr file name>
 rem
-rem   Build a ESCR script by installing it into the appropriate directory.
+rem   Build the ESCR script by installing it into the appropriate directory.
 rem
 setlocal
 call src_get "%srcdir%" "%~1.escr"
 call treename_var (cog)bat tnam
 if not exist "%tnam%" call treename_var (cog)com tnam
+if not exist "%tnam%" mkdir "%tnam%"
 echo %~1.escr
 copya -in "%~1.escr" -out "%tnam%/%~1.escr"
+del "%~1.escr"
