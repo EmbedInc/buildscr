e07ae817ca35eb21ceba8fd42bcd4de71efb5116
Olin Lathrop <OlinLathrop@users.noreply.github.com>
2019-12-24 15:20:22 -0500

    Added support for DEBUGMASTER environment variable.

--- source.bat
+++ source.bat
@@ -4,11 +4,12 @@ rem   SOURCE path
 rem
 rem   Go to (cog)source/<path>
 rem
-rem   All debug switches are also reset to off.
+rem   The DEBUG environment variable is initialized to the DEBUGMASTER
+rem   environment variable, if it exists.  Otherwise DEBUG is deleted.
 rem
 for /f "delims=" %%a in ('treename "(cog)source/%~1"') do cd /d %%a
 if errorlevel 1 exit /b 3
-set debug=
+set debug=%debugmaster%
 l
 if exist .git (
   copya -s "" -list
