0d855ccc6c5ea0fd23b66abf7daa4fc0088e12e6
Olin Lathrop <OlinLathrop@users.noreply.github.com>
2019-12-26 14:12:14 -0500

    Added SRC_SYN.

---
+++ src_syn.bat
@@ -0,0 +1,14 @@
+@echo off
+rem
+rem   SRC_SYN <.syn file name>
+rem
+rem   Build a SYN file.  On success, this will result in <name>.c and <name>.obj
+rem   in the SRC directory.
+rem
+call src_get %srcdir% %~1.syn
+if exist %~1.c del %~1.c
+if exist %~1.obj del %~1.obj
+sst %~1.syn
+if not exist %~1.c exit /b 3
+call compile_c %~1
+if not exist %~1.obj exit /b 3
