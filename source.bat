193ce59eeff4b392f4683daf2cad9e50ab586dac
Olin Lathrop <OlinLathrop@users.noreply.github.com>
2022-03-06 10:32:54 -0500

    Fixed SOURCE to do git status in subdirectory too.

--- source.bat
+++ source.bat
@@ -11,7 +11,13 @@ for /f "delims=" %%a in ('treename "(cog)source/%~1"') do cd /d %%a
 if errorlevel 1 exit /b 3
 set debug=%debugmaster%
 l
-if exist .git (
+
+setlocal
+set gitrepo=false
+if exist .git set gitrepo=true
+if exist ..\.git set gitrepo=true
+if exist ..\..\.git set gitrepo=true
+if %gitrepo%==true (
   copya -s "" -list
   git status -s -b
   )
