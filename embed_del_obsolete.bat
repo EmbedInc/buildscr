c4809047fb86f87ee7bb8980087e0197e97fcaa2
Olin Lathrop <OlinLathrop@users.noreply.github.com>
2023-01-26 13:29:00 -0500

    Changed EMBED_DEL_OBSOLETE from BAT to ESCR.

--- embed_del_obsolete.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem
-rem   Remove files that might have been installed previously, but that should
-rem   not be installed anymore.
-rem
-call delete (cog)bat/plotfilt.bat
-call delete (cog)com/plotfilt.bat
